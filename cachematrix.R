3fc950c86391f2680b40dbdf8b74266e7f4e2dc1
Wenjie <auau77@gmail.com>
2016-08-07 15:52:51 +0200

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMarix returns a list of getters and setters for a given marix, so that you can define, reset the matrix, 
+## and store the inverse value.
 
-## Write a short comment describing this function
+## setters and getters of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(invers) inv <<- invers
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix, stored in the previous function environment, or reset with the
+## set() function in the previous function environment.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
